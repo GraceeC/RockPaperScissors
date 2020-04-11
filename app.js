41e60b65c10c0b4ff7d01ec4e03da81cfaec6057
Grace Cardenas <gracecardenas@Graces-MacBook-Pro.local>
2020-04-11 08:30:05 -0700

    function created to display input value

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-
+let submitBtn = document.getElementById('button');
+let InputValue = document.getElementById('input');
 
 document.getElementById('button').addEventListener('click',() => {
 console.log('clicked');
 });
+function displayInputValue(){
+    let inputValue = document.getElementById('input');
+    return document.getElementById('playerOne').innerHTML =inputValue;
+
+
+}
