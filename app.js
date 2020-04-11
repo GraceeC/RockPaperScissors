43a4031a80c58c5377da54b0ce894fb8701eddbd
Grace Cardenas <gracecardenas@Graces-MacBook-Pro.local>
2020-04-11 07:16:18 -0700

    added behavior to js app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var arr=['rock, papper, scissors'];
-console.log('arr');
 
-var answerDisplay = prompt('Choose your option Rock, Paper or Scissors :D');
-console.log(answerDisplay);
+displayInput.addEventListener('submitButton');
+let displayInput = document.getElementById('input');
+function inputValue(){
+  console.log(displayInput);  
+}
