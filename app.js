ef9b8f256d36e9af5d89daec30147c631ca484df
Grace Cardenas <gracecardenas@Graces-MacBook-Pro.local>
2020-04-12 11:26:23 -0700

    removed previous global variables & display functions

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-let submitBtn = document.getElementById('button');
-let inputValue = document.getElementById('input');
-let text;
-
-document.getElementById('button').addEventListener('click',() => {
-console.log('clicked');
-displayInputValue()
+document.getElementById('rock').addEventListener('click', () =>{
+    console.log('rock clicked');
 });
-
-function displayInputValue(){
-    let text=inputValue.nodeValue;
-    console.log(text);
-   let player=document.getElementsByClassName('playerOne');
-   
-
-}
+document.getElementById('paper').addEventListener('click', () =>{
+    console.log('rock clicked');
+});
+document.getElementById('scissors').addEventListener('click', () =>{
+    console.log('scissors clicked');
+});
+document.getElementById('buttonOne').addEventListener('click', () =>{
+    console.log('button clicked');
+});
