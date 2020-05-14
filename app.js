f9a9d793b286beb2f3519699389ebc805afeff92
Grace C <51942103+GraceeC@users.noreply.github.com>
2020-05-13 18:32:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-document.getElementById('rock').addEventListener('click', () =>{
+window.onload = function () {
+    playersChoice();
+    computersChoice();
+    playButton();
+}
+document.getElementById('rock').addEventListener('click', () => {
     console.log('rock clicked');
 });
-document.getElementById('paper').addEventListener('click', () =>{
-    console.log('rock clicked');
+document.getElementById('paper').addEventListener('click', () => {
+    console.log('paper clicked');
 });
-document.getElementById('scissors').addEventListener('click', () =>{
+document.getElementById('scissors').addEventListener('click', () => {
     console.log('scissors clicked');
 });
-document.getElementById('buttonOne').addEventListener('click', () =>{
+document.getElementById('play').addEventListener('click', () => {
     console.log('button clicked');
 });
 
-function PlayerOneDisplay(){
-    console.log('rock');
-}
-PlayerOneDisplay();
 
-function PlayerTwoDisplay(){
-console.log('paper');
-}
-PlayerTwoDisplay();
 
-function playButton(){
+function playersChoice() {
+    let playerOneItem = document.getElementById('value-option');
+    console.log(playerOneItem);
+    playerOneItem
+}
+
+
+function computersChoice() {
+
+}
+
+
+function playButton() {
     console.log('play btn function working');
 }
-playButton();
