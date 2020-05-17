458869d8adb81a9a9a5e754ab969cb9315364ada
GraceeC <cardenasg85@yahoo.com>
2020-05-17 16:47:34 -0700

    added global variables

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+const rockItem =document.getElementById('rock');
+const paperItem = document.getElementById('paper');
+const scissorsItem = document.getElementById('scissors');
+const resetPlay = document.getElementById('reset');
+
 window.onload = function () {
     playersChoice();
     computersChoice();
     playButton();
 }
+
 document.getElementById('rock').addEventListener('click', () => {
     console.log('rock clicked');
 });
+
 document.getElementById('paper').addEventListener('click', () => {
     console.log('paper clicked');
 });
+
 document.getElementById('scissors').addEventListener('click', () => {
     console.log('scissors clicked');
 });
+
+
 document.getElementById('reset').addEventListener('click', () => {
     console.log('reset button clicked');
 });
