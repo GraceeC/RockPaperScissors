fa13055deab3e6ab4e996c304a7639be68214b1e
GraceeC <cardenasg85@yahoo.com>
2020-05-31 20:31:32 -0700

    modified html & js

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
 //global variable to grab event listener parent element
+let randomGuess = Math.floor(Math.random() * 3);
+console.log(randomGuess);
 let playersDivElement = document.querySelector('#players-choice');
-console.log(playersDivElement);
-playersDivElement.addEventListener('click', itemSelected, false );
-let computerChoice;
-let userChoice;
+let computerChoice = document.getElementById('computer-selection');
+let userChoice = document.getElementById("user-selection");
+let result = gameLogic();
 
-let results =gameLogic();
+let userSelection;
+let computerSelection;
 
-const resetPlay = document.getElementById('reset');
-const scoreKeeper = document.getElementById('score');
+playersDivElement.addEventListener("click", userChoseSelectedItem, false);
+
+window.onload = function (e) {
+  gameLogic();
+  computerSelectedItem();
+  userChoseSelectedItem(event);
+};
 
 //function using e.target to to give each btn e.target value
-function itemSelected(e){ 
-    if (e.target !== e.currentTarget){
-        let clickedItem = e.target.id;
-        console.log('button' + ' ' + clickedItem + ' ' + 'clicked' );
-    }
-    e.stopPropagation();
+let userSelectedItem = document.getElementById('user-selection');
+function userChoseSelectedItem(e) {
+  if (userSelection !== e.currentTarget) {
+    choosenItem = e.target.id;
+    userSelectedItem.innerHTML = choosenItem;
+  }
+  e.stopPropagation();
 }
 
-function gameLogic(){
-if(computerChoice === userChoice){
-    return results = ('Tie!');
+function computerSelectedItem() {
+  if (randomGuess === 1) {
+    return (result = 'rock');
+  } else if (randomGuess === 2) {
+    return (result = 'paper');
+  } else randomGuess === 3;
+  return (result = 'scissors');
 }
-gameLogic();
+console.log(result);
+computerChoice.innerHTML = computerSelection;
+
+function gameLogic() {
+  if (userChoice === computerChoice) {
+    return (result = "You both ties");
+  } else if (userChoice === "rock" && computerChoice === "paper") {
+    return (result = "You lost, Try again!");
+  } else if (userChoice === "rock" && computerChoice === "scissors") {
+    return (result = "Congratgulations you won!");
+  } else if (userChoice === "scissors" && computerChoice === "rock") {
+    return (result = "You lost, Try again!");
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    return (result = "Congratgulations you won!");
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    return (result = "Congratgulations you won!");
+  } else if (userChoice === "paper" && computerChoice === "scissors")
+    return (result = "You lost, try again");
 }
