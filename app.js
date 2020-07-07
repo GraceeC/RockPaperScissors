2418fe3f289dbdcb916408a74d25cbbe4fc1c77a
GraceeC <cardenasg85@yahoo.com>
2020-07-07 14:44:45 -0700

    updated coded

--- app.js
+++ app.js
@@ -1,65 +1,30 @@
-let randomGuess = Math.round(Math.random() * (3));
-let playersDivElement = document.querySelector('#players-choice');
-let userChoiceTag = document.getElementById('user-selection');
-let computerHeaderTag = document.getElementById('computer-selection');
+let userPlay;
+let computerPlay;
 
-let computerSelection;
-let userSelection;
-let result;
+window.onload = function () {
+  startGame();
+  computerChoice();
+}
 
-let playButtonPress = document.getElementById('play');
-playButtonPress.addEventListener('click', function(){
-console.log(playButtonPress,'button clicked');
-window.document.location = './play.html';
-});
+let startGame = function () {
+  playButton = document.getElementById('play-button');
+  playButton.addEventListener('click', function () {
+    console.log(playButton, 'button clicked');
+    window.document.location = './play.html';
+  });
+}
 
+let computerChoice = function () {
+  let optionArray = ['rock', 'scissors', 'paper'];
+  let randomGuess = optionArray[Math.floor(Math.random() * optionArray.length)];
+  console.log(randomGuess);
 
-// playersDivElement.addEventListener('click', userChoseSelectedItem, false);
-// //function giving logic to user
-// function userChoseSelectedItem(e) {
-//     userSelection = e.target;
-    
-//   if(computerSelection !== computerSelection){
-//       itemSelected = computerSelection;
-//       computerHeaderTag.innerHTML = itemSelected;
-//   } 
-//   e.stopPropagation();
-// }
+  let computerSelectedItem = document.getElementById('computer-selection');
+  computerSelectedItem.innerText = randomGuess
+}
 
-// console.log(computerSelection)
+let userChoice = function () {
+  let userSelectedItem = document.querySelectorAll('.choice');
 
 
-
-
-
-
-// function computerSelectedItem() {
-//   if (randomGuess === 1) {
-//     return computerSelection = 'rock'
-//   } else if (randomGuess === 2) {
-//     return computerSelection = 'paper'
-//   } else (randomGuess === 3);
-//   return computerSelection = 'scissors'
-// }
-
-
-// computerHeaderTag.innerHTML = computerSelection;
-
-
-// function gameLogic(e) {
-//   if (computerHeaderTag === userSelection) {
-//     return result='Its a tie'
-//   } else if (computerHeaderTag === 'rock' && userSelection === 'paper') {
-//     return result = 'You lost, Try again!';
-//   } else if (computerHeaderTag === 'rock' && userSelection === 'scissors') {
-//     return result = 'Congratgulations you won!';
-//   } else if (computerHeaderTag === 'scissors' && userSelection === 'rock') {
-//     return result = 'You lost, Try again!';
-//   } else if (computerHeaderTag === 'scissors' && userSelection === 'paper') {
-//     return result = 'Congratgulations you won!';
-//   } else if (computerHeaderTag === 'paper' && userSelection === 'rock') {
-//     return result = 'Congratgulations you won!';
-//   } else if (computerHeaderTag === 'paper' && userSelection === 'scissors'){ 
-//     return result = 'You lost, try again';
-// }
-// }
+}
